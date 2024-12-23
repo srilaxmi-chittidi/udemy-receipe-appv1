e9074984831e0a90b1009d44aadb8cb9686afe00
srilaxmi-chittidi <laxmi.chittidi@gmail.com>
2024-12-22 20:10:27 -0500

    Initial Commit

---
+++ recipe-app/src/main/java/com/udemy/recipe_app/domain/Recipe.java
@@ -0,0 +1,126 @@
+package com.udemy.recipe_app.domain;
+
+import jakarta.persistence.*;
+import org.hibernate.engine.internal.Cascade;
+
+import java.util.Set;
+
+@Entity
+public class Recipe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public Integer getCookTime() {
+        return cookTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+//    public Difficulty getDifficulty() {
+//        return difficulty;
+//    }
+
+//    public void setDifficulty(Difficulty difficulty) {
+//        this.difficulty = difficulty;
+//    }
+
+    public Byte[] getImages() {
+        return images;
+    }
+
+    public void setImages(Byte[] images) {
+        this.images = images;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredientSet() {
+        return ingredientSet;
+    }
+
+    public void setIngredientSet(Set<Ingredient> ingredientSet) {
+        this.ingredientSet = ingredientSet;
+    }
+
+    private String directions;
+    //private Difficulty difficulty;
+    @Lob
+    private Byte[] images;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
+    private Set<Ingredient> ingredientSet;
+
+}
